4df05cc88ea525051105a1d3e48600b082493e1b
alek <wait82985@gmail.com>
2023-07-09 21:27:01 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,16 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
-        //TODO
-        return 0;
+        int globalMax = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+            currentSum += nums[i];
+            globalMax = Math.max(globalMax, currentSum);
+        }
+
+        return globalMax;
     }
 }
